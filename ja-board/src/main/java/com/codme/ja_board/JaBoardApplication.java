99588cc2654a364114ea4a6895cbd505fa18cad9
Carlos Andres Lopez <andreslrz@gmail.com>
2024-10-06 17:44:21 -0500

    Initial commit

---
+++ ja-board/src/main/java/com/codme/ja_board/JaBoardApplication.java
@@ -0,0 +1,13 @@
+package com.codme.ja_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaBoardApplication.class, args);
+	}
+
+}
---
+++ ja-board/src/test/java/com/codme/ja_board/JaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codme.ja_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
